3260c8ae736b8f7cefa30571867ecc2ef36a2fcb
bilalafzal97 <bilalafzal245256@gmail.com>
2024-09-10 14:26:12 +0500

    Audit fix: Precision Error

--- programs/soga-node-sale/src/instructions/buy_with_token.rs
+++ programs/soga-node-sale/src/instructions/buy_with_token.rs
@@ -192,7 +192,8 @@ pub fn handle_buy_with_token<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, 
 
     let pyth_expo: u64 = 10_u64.pow(price.exponent.abs().try_into().unwrap());
     let pyth_price: u64 = u64::try_from(price.price).unwrap();
-    let price_in_lamport: u64 = LAMPORTS_PER_SOL.checked_mul(pyth_expo).unwrap().checked_div(pyth_price).unwrap().checked_mul(price_in_usd).unwrap();
+    let decimal_base_lamport: u64 = 10_u64.pow(sale_phase_payment_token_detail.decimals.try_into().unwrap());
+    let price_in_lamport: u64 = decimal_base_lamport.checked_mul(pyth_expo).unwrap().checked_div(pyth_price).unwrap().checked_mul(price_in_usd).unwrap();
 
     let mut user_discount_in_usd: u64 = 0;
     let mut user_discount_in_lamport: u64 = 0;
