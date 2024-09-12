d38ddbd7f5629f656605c1747789ba53a67fe263
codebender828 <codebender828@gmail.com>
2024-09-12 10:39:33 +0300

    chore: prep for deployment

--- programs/soga-node-sale/src/lib.rs
+++ programs/soga-node-sale/src/lib.rs
@@ -1,15 +1,14 @@
 use anchor_lang::prelude::*;
 
-
 use instructions::*;
 
+mod error;
+mod events;
 mod instructions;
 mod states;
-mod events;
-mod error;
 mod utils;
 
-declare_id!("4cQkDBqFZmuiuwsQnbrFEU5kcRUTCYS1agegC3yLVYQq");
+declare_id!("8bN6BdztrKwNrxqq6BmRjAqM2bQT7wtMvXkY5JuR6sst");
 
 #[program]
 pub mod soga_node_sale {
@@ -19,93 +18,251 @@ pub mod soga_node_sale {
         handle_initialize(ctx)
     }
 
-    pub fn initialize_sale_phase(ctx: Context<InitializeSalePhaseInputAccounts>,
-                                 _sale_config_bump: u8, sale_phase_name: String,
-                                 total_tiers: u32, name: String, symbol: String, metadata_base_uri: String,
-                                 price_feed_id: String,
+    pub fn initialize_sale_phase(
+        ctx: Context<InitializeSalePhaseInputAccounts>,
+        _sale_config_bump: u8,
+        sale_phase_name: String,
+        total_tiers: u32,
+        name: String,
+        symbol: String,
+        metadata_base_uri: String,
+        price_feed_id: String,
     ) -> Result<()> {
-        handle_initialize_sale_phase(ctx, _sale_config_bump, sale_phase_name, total_tiers, name, symbol, metadata_base_uri, price_feed_id)
+        handle_initialize_sale_phase(
+            ctx,
+            _sale_config_bump,
+            sale_phase_name,
+            total_tiers,
+            name,
+            symbol,
+            metadata_base_uri,
+            price_feed_id,
+        )
     }
 
-    pub fn initialize_sale_phase_tier(ctx: Context<InitializeSalePhaseTierInputAccounts>,
-                                      _sale_phase_detail_bump: u8, sale_phase_name: String,
-                                      tier_id: String, price: u64, quantity: u64, mint_limit: u64,
-                                      collection_name: String, collection_symbol: String, collection_url: String,
-                                      whitelist_quantity: u64,
+    pub fn initialize_sale_phase_tier(
+        ctx: Context<InitializeSalePhaseTierInputAccounts>,
+        _sale_phase_detail_bump: u8,
+        sale_phase_name: String,
+        tier_id: String,
+        price: u64,
+        quantity: u64,
+        mint_limit: u64,
+        collection_name: String,
+        collection_symbol: String,
+        collection_url: String,
+        whitelist_quantity: u64,
     ) -> Result<()> {
-        handle_initialize_sale_phase_tier(ctx, _sale_phase_detail_bump, sale_phase_name, tier_id, price, quantity, mint_limit,
-                                          collection_name, collection_symbol, collection_url, whitelist_quantity)
+        handle_initialize_sale_phase_tier(
+            ctx,
+            _sale_phase_detail_bump,
+            sale_phase_name,
+            tier_id,
+            price,
+            quantity,
+            mint_limit,
+            collection_name,
+            collection_symbol,
+            collection_url,
+            whitelist_quantity,
+        )
     }
 
-    pub fn buy<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, BuyInputAccounts<'info>>,
-                                  _sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8,
-                                  sale_phase_name: String, tier_id: String, order_id: String, quantity: u64,
-                                  allow_full_discount: bool, full_discount: u16, allow_half_discount: bool, half_discount: u16,
-                                  is_whitelist: bool, allow_user_discount: bool, user_discount: u16,
+    pub fn buy<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, BuyInputAccounts<'info>>,
+        _sale_phase_detail_bump: u8,
+        _sale_phase_tier_detail_bump: u8,
+        sale_phase_name: String,
+        tier_id: String,
+        order_id: String,
+        quantity: u64,
+        allow_full_discount: bool,
+        full_discount: u16,
+        allow_half_discount: bool,
+        half_discount: u16,
+        is_whitelist: bool,
+        allow_user_discount: bool,
+        user_discount: u16,
     ) -> Result<()> {
-        handle_buy(ctx, _sale_phase_detail_bump, _sale_phase_tier_detail_bump,
-                   sale_phase_name, tier_id, order_id, quantity, allow_full_discount, full_discount, allow_half_discount, half_discount,
-                   is_whitelist, allow_user_discount, user_discount)
+        handle_buy(
+            ctx,
+            _sale_phase_detail_bump,
+            _sale_phase_tier_detail_bump,
+            sale_phase_name,
+            tier_id,
+            order_id,
+            quantity,
+            allow_full_discount,
+            full_discount,
+            allow_half_discount,
+            half_discount,
+            is_whitelist,
+            allow_user_discount,
+            user_discount,
+        )
     }
 
-    pub fn airdrop<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, AirdropInputAccounts<'info>>,
-                                      _sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8,
-                                      _collection_mint_account_bump: u8, sale_phase_name: String, tier_id: String, token_id: String,
+    pub fn airdrop<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, AirdropInputAccounts<'info>>,
+        _sale_phase_detail_bump: u8,
+        _sale_phase_tier_detail_bump: u8,
+        _collection_mint_account_bump: u8,
+        sale_phase_name: String,
+        tier_id: String,
+        token_id: String,
     ) -> Result<()> {
-        handle_airdrop(ctx, _sale_phase_detail_bump, _sale_phase_tier_detail_bump, _collection_mint_account_bump,
-                       sale_phase_name, tier_id, token_id)
+        handle_airdrop(
+            ctx,
+            _sale_phase_detail_bump,
+            _sale_phase_tier_detail_bump,
+            _collection_mint_account_bump,
+            sale_phase_name,
+            tier_id,
+            token_id,
+        )
     }
 
-    pub fn update_sale_phase(ctx: Context<UpdateSalePhaseInputAccounts>,
-                             _sale_phase_detail_bump: u8, sale_phase_name: String,
-                             name: String, symbol: String, metadata_base_uri: String,
-                             buy_enable: bool, buy_with_token_enable: bool, airdrop_enable: bool,
-                             price_feed_id: String,
+    pub fn update_sale_phase(
+        ctx: Context<UpdateSalePhaseInputAccounts>,
+        _sale_phase_detail_bump: u8,
+        sale_phase_name: String,
+        name: String,
+        symbol: String,
+        metadata_base_uri: String,
+        buy_enable: bool,
+        buy_with_token_enable: bool,
+        airdrop_enable: bool,
+        price_feed_id: String,
     ) -> Result<()> {
-        handle_update_sale_phase(ctx, _sale_phase_detail_bump, sale_phase_name, name, symbol, metadata_base_uri,
-                                 buy_enable, buy_with_token_enable, airdrop_enable, price_feed_id)
+        handle_update_sale_phase(
+            ctx,
+            _sale_phase_detail_bump,
+            sale_phase_name,
+            name,
+            symbol,
+            metadata_base_uri,
+            buy_enable,
+            buy_with_token_enable,
+            airdrop_enable,
+            price_feed_id,
+        )
     }
 
-    pub fn update_sale_phase_tier(ctx: Context<UpdateSalePhaseTierInputAccounts>,
-                                  _sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8, sale_phase_name: String,
-                                  tier_id: String, price: u64, mint_limit: u64,
-                                  buy_enable: bool, buy_with_token_enable: bool, airdrop_enable: bool,
+    pub fn update_sale_phase_tier(
+        ctx: Context<UpdateSalePhaseTierInputAccounts>,
+        _sale_phase_detail_bump: u8,
+        _sale_phase_tier_detail_bump: u8,
+        sale_phase_name: String,
+        tier_id: String,
+        price: u64,
+        mint_limit: u64,
+        buy_enable: bool,
+        buy_with_token_enable: bool,
+        airdrop_enable: bool,
     ) -> Result<()> {
-        handle_update_sale_phase_tier(ctx, _sale_phase_detail_bump, _sale_phase_tier_detail_bump, sale_phase_name,
-                                      tier_id, price, mint_limit, buy_enable, buy_with_token_enable, airdrop_enable)
+        handle_update_sale_phase_tier(
+            ctx,
+            _sale_phase_detail_bump,
+            _sale_phase_tier_detail_bump,
+            sale_phase_name,
+            tier_id,
+            price,
+            mint_limit,
+            buy_enable,
+            buy_with_token_enable,
+            airdrop_enable,
+        )
     }
 
-    pub fn initialize_sale_phase_token_payment(ctx: Context<InitializeSalePhasePaymentTokenInputAccounts>,
-                                               _sale_phase_detail_bump: u8, sale_phase_name: String,
-                                               price_feed_id: String,
+    pub fn initialize_sale_phase_token_payment(
+        ctx: Context<InitializeSalePhasePaymentTokenInputAccounts>,
+        _sale_phase_detail_bump: u8,
+        sale_phase_name: String,
+        price_feed_id: String,
     ) -> Result<()> {
-        handle_initialize_sale_phase_token_payment(ctx, _sale_phase_detail_bump, sale_phase_name, price_feed_id)
+        handle_initialize_sale_phase_token_payment(
+            ctx,
+            _sale_phase_detail_bump,
+            sale_phase_name,
+            price_feed_id,
+        )
     }
 
-    pub fn update_sale_phase_token_payment(ctx: Context<UpdateSalePhasePaymentTokenInputAccounts>,
-                                           _sale_phase_detail_bump: u8, _sale_phase_payment_token_detail_bump: u8,
-                                           sale_phase_name: String, enable: bool, price_feed_id: String,
+    pub fn update_sale_phase_token_payment(
+        ctx: Context<UpdateSalePhasePaymentTokenInputAccounts>,
+        _sale_phase_detail_bump: u8,
+        _sale_phase_payment_token_detail_bump: u8,
+        sale_phase_name: String,
+        enable: bool,
+        price_feed_id: String,
     ) -> Result<()> {
-        handle_update_sale_phase_token_payment(ctx, _sale_phase_detail_bump, _sale_phase_payment_token_detail_bump, sale_phase_name,
-                                               enable, price_feed_id)
+        handle_update_sale_phase_token_payment(
+            ctx,
+            _sale_phase_detail_bump,
+            _sale_phase_payment_token_detail_bump,
+            sale_phase_name,
+            enable,
+            price_feed_id,
+        )
     }
 
-    pub fn buy_with_token<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, BuyWithTokenInputAccounts<'info>>,
-                                             _sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8,
-                                             sale_phase_name: String, tier_id: String, order_id: String,
-                                             quantity: u64, allow_full_discount: bool, full_discount: u16, allow_half_discount: bool, half_discount: u16,
-                                             is_whitelist: bool, allow_user_discount: bool, user_discount: u16,
+    pub fn buy_with_token<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, BuyWithTokenInputAccounts<'info>>,
+        _sale_phase_detail_bump: u8,
+        _sale_phase_tier_detail_bump: u8,
+        sale_phase_name: String,
+        tier_id: String,
+        order_id: String,
+        quantity: u64,
+        allow_full_discount: bool,
+        full_discount: u16,
+        allow_half_discount: bool,
+        half_discount: u16,
+        is_whitelist: bool,
+        allow_user_discount: bool,
+        user_discount: u16,
     ) -> Result<()> {
-        handle_buy_with_token(ctx, _sale_phase_detail_bump, _sale_phase_tier_detail_bump,
-                              sale_phase_name, tier_id, order_id, quantity, allow_full_discount, full_discount, allow_half_discount, half_discount,
-                              is_whitelist, allow_user_discount, user_discount)
+        handle_buy_with_token(
+            ctx,
+            _sale_phase_detail_bump,
+            _sale_phase_tier_detail_bump,
+            sale_phase_name,
+            tier_id,
+            order_id,
+            quantity,
+            allow_full_discount,
+            full_discount,
+            allow_half_discount,
+            half_discount,
+            is_whitelist,
+            allow_user_discount,
+            user_discount,
+        )
     }
 
-    pub fn file_order<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, FillOrderInputAccounts<'info>>,
-                                         _sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8, _collection_mint_account_bump: u8,
-                                         _user_detail_bump: u8, _order_detail_bump: u8, sale_phase_name: String, tier_id: String, token_id: String, order_id: String,
+    pub fn file_order<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, FillOrderInputAccounts<'info>>,
+        _sale_phase_detail_bump: u8,
+        _sale_phase_tier_detail_bump: u8,
+        _collection_mint_account_bump: u8,
+        _user_detail_bump: u8,
+        _order_detail_bump: u8,
+        sale_phase_name: String,
+        tier_id: String,
+        token_id: String,
+        order_id: String,
     ) -> Result<()> {
-        handle_file_order(ctx, _sale_phase_detail_bump, _sale_phase_tier_detail_bump, _collection_mint_account_bump, _user_detail_bump,
-                          _order_detail_bump, sale_phase_name, tier_id, token_id, order_id)
+        handle_file_order(
+            ctx,
+            _sale_phase_detail_bump,
+            _sale_phase_tier_detail_bump,
+            _collection_mint_account_bump,
+            _user_detail_bump,
+            _order_detail_bump,
+            sale_phase_name,
+            tier_id,
+            token_id,
+            order_id,
+        )
     }
 }
--- programs/soga-raffle-ticket/src/lib.rs
+++ programs/soga-raffle-ticket/src/lib.rs
@@ -2,13 +2,13 @@ use anchor_lang::prelude::*;
 
 use instructions::*;
 
+mod error;
+mod events;
 mod instructions;
 mod states;
-mod events;
-mod error;
 mod utils;
 
-declare_id!("GeYppgZZK83wKvfbwpnuipDENcYkw4uabZqvfHWf6Rfq");
+declare_id!("4FrgeLwpX7EUf4nriVP6TvTiCoi4WVUQ7wPVw8hESK7B");
 
 #[program]
 pub mod soga_raffle_ticket {
@@ -18,43 +18,170 @@ pub mod soga_raffle_ticket {
         handle_initialize(ctx)
     }
 
-    pub fn create_ticket_config(ctx: Context<CreateTicketInputAccounts>, _config_bump: u8, ticket_config_name: String, winner_ticket_limit: u64) -> Result<()> {
+    pub fn create_ticket_config(
+        ctx: Context<CreateTicketInputAccounts>,
+        _config_bump: u8,
+        ticket_config_name: String,
+        winner_ticket_limit: u64,
+    ) -> Result<()> {
         handle_create_ticket_config(ctx, _config_bump, ticket_config_name, winner_ticket_limit)
     }
 
-    pub fn update_ticket_config(ctx: Context<UpdateTicketConfigInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, ticket_purchase_enable: bool, ticket_refund_enable: bool, winner_ticket_limit: u64) -> Result<()> {
-        handle_update_ticket_config(ctx, ticket_config_name, _ticket_config_bump, ticket_purchase_enable, ticket_refund_enable, winner_ticket_limit)
+    pub fn update_ticket_config(
+        ctx: Context<UpdateTicketConfigInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        ticket_purchase_enable: bool,
+        ticket_refund_enable: bool,
+        winner_ticket_limit: u64,
+    ) -> Result<()> {
+        handle_update_ticket_config(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            ticket_purchase_enable,
+            ticket_refund_enable,
+            winner_ticket_limit,
+        )
     }
 
-    pub fn create_payment_config(ctx: Context<CreatePaymentConfigInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, ticket_price: u64, refund_amount: u64) -> Result<()> {
-        handle_create_payment_config(ctx, ticket_config_name, _ticket_config_bump, ticket_price, refund_amount)
+    pub fn create_payment_config(
+        ctx: Context<CreatePaymentConfigInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        ticket_price: u64,
+        refund_amount: u64,
+    ) -> Result<()> {
+        handle_create_payment_config(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            ticket_price,
+            refund_amount,
+        )
     }
 
-    pub fn update_payment_config(ctx: Context<UpdatePaymentConfigInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, _payment_config_bump: u8, ticket_price: u64, refund_amount: u64, enable: bool, ticket_purchase_enable: bool, ticket_refund_enable: bool) -> Result<()> {
-        handle_update_payment_config(ctx, ticket_config_name, _ticket_config_bump, _payment_config_bump, ticket_price, refund_amount, enable, ticket_purchase_enable, ticket_refund_enable)
+    pub fn update_payment_config(
+        ctx: Context<UpdatePaymentConfigInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        _payment_config_bump: u8,
+        ticket_price: u64,
+        refund_amount: u64,
+        enable: bool,
+        ticket_purchase_enable: bool,
+        ticket_refund_enable: bool,
+    ) -> Result<()> {
+        handle_update_payment_config(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            _payment_config_bump,
+            ticket_price,
+            refund_amount,
+            enable,
+            ticket_purchase_enable,
+            ticket_refund_enable,
+        )
     }
 
-    pub fn add_payment_supply(ctx: Context<AddPaymentSupplyInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, _payment_config_bump: u8, amount: u64) -> Result<()> {
-        handle_add_payment_supply(ctx, ticket_config_name, _ticket_config_bump, _payment_config_bump, amount)
+    pub fn add_payment_supply(
+        ctx: Context<AddPaymentSupplyInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        _payment_config_bump: u8,
+        amount: u64,
+    ) -> Result<()> {
+        handle_add_payment_supply(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            _payment_config_bump,
+            amount,
+        )
     }
 
-    pub fn withdraw_payment_supply(ctx: Context<WithdrawPaymentSupplyInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, payment_config_bump: u8, amount: u64) -> Result<()> {
-        handle_withdraw_payment_supply(ctx, ticket_config_name, _ticket_config_bump, payment_config_bump, amount)
+    pub fn withdraw_payment_supply(
+        ctx: Context<WithdrawPaymentSupplyInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        payment_config_bump: u8,
+        amount: u64,
+    ) -> Result<()> {
+        handle_withdraw_payment_supply(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            payment_config_bump,
+            amount,
+        )
     }
 
-    pub fn buy_ticket(ctx: Context<BuyTicketInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, _payment_config_bump: u8, quantity: u64) -> Result<()> {
-        handle_buy_ticket(ctx, ticket_config_name, _ticket_config_bump, _payment_config_bump, quantity)
+    pub fn buy_ticket(
+        ctx: Context<BuyTicketInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        _payment_config_bump: u8,
+        quantity: u64,
+    ) -> Result<()> {
+        handle_buy_ticket(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            _payment_config_bump,
+            quantity,
+        )
     }
 
-    pub fn add_ticket_winner(ctx: Context<AddWinnerTicketInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, _payment_config_bump: u8, _user_config_bump: u8, _user_payment_config_bump: u8, quantity: u64) -> Result<()> {
-        handle_add_ticket_winner(ctx, ticket_config_name, _ticket_config_bump, _payment_config_bump, _user_config_bump, _user_payment_config_bump, quantity)
+    pub fn add_ticket_winner(
+        ctx: Context<AddWinnerTicketInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        _payment_config_bump: u8,
+        _user_config_bump: u8,
+        _user_payment_config_bump: u8,
+        quantity: u64,
+    ) -> Result<()> {
+        handle_add_ticket_winner(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            _payment_config_bump,
+            _user_config_bump,
+            _user_payment_config_bump,
+            quantity,
+        )
     }
 
-    pub fn add_claimed_ticket(ctx: Context<AddClaimedTicketInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, _user_config_bump: u8) -> Result<()> {
-        handle_add_claimed_ticket(ctx, ticket_config_name, _ticket_config_bump, _user_config_bump)
+    pub fn add_claimed_ticket(
+        ctx: Context<AddClaimedTicketInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        _user_config_bump: u8,
+    ) -> Result<()> {
+        handle_add_claimed_ticket(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            _user_config_bump,
+        )
     }
 
-    pub fn refund_ticket(ctx: Context<RefundTicketInputAccounts>, ticket_config_name: String, _ticket_config_bump: u8, payment_config_bump: u8, _user_config_bump: u8, _user_payment_config_bump: u8) -> Result<()> {
-        handle_refund_ticket(ctx, ticket_config_name, _ticket_config_bump, payment_config_bump, _user_config_bump, _user_payment_config_bump)
+    pub fn refund_ticket(
+        ctx: Context<RefundTicketInputAccounts>,
+        ticket_config_name: String,
+        _ticket_config_bump: u8,
+        payment_config_bump: u8,
+        _user_config_bump: u8,
+        _user_payment_config_bump: u8,
+    ) -> Result<()> {
+        handle_refund_ticket(
+            ctx,
+            ticket_config_name,
+            _ticket_config_bump,
+            payment_config_bump,
+            _user_config_bump,
+            _user_payment_config_bump,
+        )
     }
 }
