e73843189c8841918fa165aefe95f07c96554b0f
codebender828 <codebender828@gmail.com>
2024-09-12 15:38:46 +0300

    fix(ix:update_sale_phase_tier): update buy_with_token_enable field

--- programs/soga-node-sale/src/instructions/update_sale_phase_tier.rs
+++ programs/soga-node-sale/src/instructions/update_sale_phase_tier.rs
@@ -1,18 +1,13 @@
 use anchor_lang::prelude::*;
 
 use crate::states::{
-    SOGA_NODE_SALE_PHASE_DETAIL_ACCOUNT_PREFIX,
-    SogaNodeSalePhaseDetailAccount,
-    SOGA_NODE_SALE_PHASE_TIER_DETAIL_ACCOUNT_PREFIX,
-    SogaNodeSalePhaseTierDetailAccount,
+    SogaNodeSalePhaseDetailAccount, SogaNodeSalePhaseTierDetailAccount,
+    SOGA_NODE_SALE_PHASE_DETAIL_ACCOUNT_PREFIX, SOGA_NODE_SALE_PHASE_TIER_DETAIL_ACCOUNT_PREFIX,
 };
 
-use crate::events::{UpdateSalePhaseTierEvent};
+use crate::events::UpdateSalePhaseTierEvent;
 
-use crate::utils::{
-    check_signing_authority,
-    check_value_is_zero,
-};
+use crate::utils::{check_signing_authority, check_value_is_zero};
 
 #[derive(Accounts)]
 #[instruction(_sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8, sale_phase_name: String, tier_id: String)]
@@ -47,14 +42,22 @@ pub struct UpdateSalePhaseTierInputAccounts<'info> {
     pub rent: Sysvar<'info, Rent>,
 }
 
-pub fn handle_update_sale_phase_tier(ctx: Context<UpdateSalePhaseTierInputAccounts>,
-                                     _sale_phase_detail_bump: u8, _sale_phase_tier_detail_bump: u8, sale_phase_name: String,
-                                     tier_id: String, price: u64, mint_limit: u64,
-                                     buy_enable: bool, buy_with_token_enable: bool, airdrop_enable: bool,
+pub fn handle_update_sale_phase_tier(
+    ctx: Context<UpdateSalePhaseTierInputAccounts>,
+    _sale_phase_detail_bump: u8,
+    _sale_phase_tier_detail_bump: u8,
+    sale_phase_name: String,
+    tier_id: String,
+    price: u64,
+    mint_limit: u64,
+    buy_enable: bool,
+    buy_with_token_enable: bool,
+    airdrop_enable: bool,
 ) -> Result<()> {
     let timestamp = Clock::get().unwrap().unix_timestamp;
 
-    let sale_phase_detail: &Box<Account<SogaNodeSalePhaseDetailAccount>> = &ctx.accounts.sale_phase_detail;
+    let sale_phase_detail: &Box<Account<SogaNodeSalePhaseDetailAccount>> =
+        &ctx.accounts.sale_phase_detail;
 
     // Checks
 
@@ -62,13 +65,18 @@ pub fn handle_update_sale_phase_tier(ctx: Context<UpdateSalePhaseTierInputAccoun
 
     check_value_is_zero(mint_limit as usize)?;
 
-    check_signing_authority(sale_phase_detail.signing_authority, ctx.accounts.signing_authority.key())?;
+    check_signing_authority(
+        sale_phase_detail.signing_authority,
+        ctx.accounts.signing_authority.key(),
+    )?;
 
-    let sale_phase_tier_detail: &mut Box<Account<SogaNodeSalePhaseTierDetailAccount>> = &mut ctx.accounts.sale_phase_tier_detail;
+    let sale_phase_tier_detail: &mut Box<Account<SogaNodeSalePhaseTierDetailAccount>> =
+        &mut ctx.accounts.sale_phase_tier_detail;
     sale_phase_tier_detail.last_block_timestamp = timestamp;
     sale_phase_tier_detail.price = price;
     sale_phase_tier_detail.mint_limit = mint_limit;
     sale_phase_tier_detail.buy_enable = buy_enable;
+    sale_phase_tier_detail.buy_with_token_enable = buy_with_token_enable;
     sale_phase_tier_detail.airdrop_enable = airdrop_enable;
 
     // Event
